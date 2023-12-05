3a080c5a18f2aef6911dc514ca544ed648a6e259
2z75 <zinedine.chader@mail.com>
2023-12-05 12:39:48 +0100

    quete 12

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -6,18 +6,22 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Episode;
+use Faker\Factory;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create();
+
+
         foreach (ProgramFixtures::PROGRAMS as $key => $programData) {
             for ($i= 1; $i <= 5; $i++) {
-                for($e = 1; $e <= 5; $e++) {
+                for($e = 1; $e <= 10; $e++) {
         $episode = new Episode();
-        $episode->setTitle("Titre test")
-                ->setNumber($e)
-                ->setSynopsis("Résumé de l'episode" . $e )
+        $episode->setTitle($faker->word())
+                ->setNumber($faker->numberBetween(1, 10))
+                ->setSynopsis($faker->paragraphs(3, true) . $e )
                 ->setSeason($this->getReference('season_' .$i . '_' . $key));
 
         $manager->persist($episode);
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -6,18 +6,22 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Season;
-
+use Faker\Factory;
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
+
+        $faker = Factory::create();
+
+
         // Assume that 'program_existing' is the reference for your existing program
         foreach (ProgramFixtures::PROGRAMS as $key => $programData) {
             for ($i= 1; $i <= 5; $i++) {
         $season = new Season();
-        $season ->setNumber($i)
-                ->setYear(2012)
-                ->setDescription('on improvise')
+        $season ->setNumber($faker->numberBetween(1, 5))
+                ->setYear($faker->year())
+                ->setDescription($faker->paragraphs(3, true))
                 ->setProgram($this->getReference('program_' . $key));
         $this->addReference('season_' . $i . '_' . $key, $season);
         $manager->persist($season);
